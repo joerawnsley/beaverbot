b0f0ba2bbc89fe8be128f44427392942efc2c63b
Wren Hawthorne <wren.hawthorne@madetech.com>
2026-07-22 15:57:13 +0100

    update test

--- tests/log-hours.test.js
+++ tests/log-hours.test.js
@@ -1,79 +1,99 @@
-const { test, expect } = require("@playwright/test");
+import { test, expect } from "@playwright/test";
+import * as OTPAuth from "otpauth";
 
 const logEntries = require("../data.json");
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
+const MFA_SECRET = process.env.MFA_SECRET;
 
 test("Log 'On The Job' Hours", async ({ page }) => {
-  const today = new Date().getDate().toString();
-
-  await page.goto("https://smartassessor.co.uk/Account");
+  await page.goto("https://education.oneadvanced.com");
   await page.getByRole("textbox", { name: "Username" }).fill(USERNAME);
+  await page.getByRole("button", { name: "Continue" }).click();
+
   await page.getByRole("textbox", { name: "Password" }).fill(PASSWORD);
   await page.getByRole("button", { name: "Log In" }).click();
 
-  await page.getByRole("link", { name: /DEVOPS ENGINEER/ }).click();
-  await page.getByRole("link", { name: "Time Log" }).click();
-
-  for (const entry of logEntries) {
-    console.log(`Processing: ${entry.date}`);
-
-    await page.getByRole("button", { name: "Add New Timelog Entry" }).click();
-
-    const logFrame = page.frameLocator("#formModalFrame");
-
-    await logFrame
-      .getByRole("textbox", { name: "Select Activity Date" })
-      .fill(entry.date);
-
-    await logFrame
-      .getByLabel("Select Activity Type")
-      .selectOption({ label: "Gaining technical experience by doing my job" });
-
-    await logFrame
-      .getByLabel("Select Course")
-      .selectOption({ label: "DEVOPS ENGINEER (2021) 548" });
-
-    await logFrame
-      .getByLabel("Select Assessor")
-      .selectOption({ label: "Dom Patmore" });
-
-    await logFrame
-      .getByLabel("Was it on the Job?")
-      .selectOption({ label: "On the job" });
-
-    await logFrame
-      .getByRole("textbox", { name: "Time Spent on Activity" })
-      .fill(entry.timeSpent);
-
-    await logFrame
-      .getByRole("textbox", { name: "Activity Start Time" })
-      .fill(entry.startTime);
-
-    await logFrame
-      .getByRole("textbox", { name: "What impact has this activity" })
-      .fill("N/A");
-
-    await logFrame.getByRole("button", { name: "Add Activity" }).click();
-
-    await page.locator("#formModalFrame").waitFor({ state: "hidden" });
-
-    const timesheetTable = page.locator("table.timesheet");
-
-    const expectedRow = timesheetTable
-      .locator("tr")
-      .filter({
-        hasText: entry.date,
-      })
-      .filter({
-        hasText: entry.timeSpent,
+  // MFA
+  const totp = MFA_SECRET.startsWith("otpauth://")
+    ? OTPAuth.URI.parse(MFA_SECRET)
+    : new OTPAuth.TOTP({
+        algorithm: "SHA1",
+        digits: 6,
+        period: 30,
+        secret: MFA_SECRET,
       });
 
+  const otpCode = totp.generate();
+  await page.getByRole("textbox", { name: "Verification code" }).fill(otpCode);
+  await page.getByRole("button", { name: "Submit" }).click();
+
+  // Navigate to timelog
+  await page.getByRole("link", { name: "Timelog" }).click();
+
+  const entriesContainer = page.locator(".mds-accordion");
+
+  // Add entries
+  for (const entry of logEntries) {
+    const formattedTimeSpent = `${entry.hoursSpent}h`;
+    const displayDate = entry.date.replace(/\/20(\d{2})$/, "/$1");
+
+    // Check if the entry is already present in the accordion list
+    const existingRow = entriesContainer
+      .locator(".mds-accordion-content__content > div")
+      .filter({ hasText: displayDate })
+      .filter({ hasText: formattedTimeSpent });
+
+    if ((await existingRow.count()) > 0) {
+      console.log(
+        `⏩ ${entry.date} (${formattedTimeSpent}) already exists. Skipping...`,
+      );
+      continue;
+    }
+
+    console.log(`Processing: ${entry.date}`);
+
+    await page.getByRole("button", { name: "Add Hours" }).click();
+
+    // Target modal
+    const addHoursModal = page.getByRole("dialog", {
+      name: "Add Off-the-Job Hours",
+    });
+
+    await addHoursModal.getByLabel("Activity date").fill(entry.date);
+    await page.keyboard.press("Escape");
+    await addHoursModal.getByLabel("Time started").fill(entry.startTime);
+    await page.keyboard.press("Escape");
+
+    await addHoursModal
+      .getByRole("spinbutton", { name: "Hours" })
+      .fill(entry.hoursSpent);
+
+    if (entry.minsSpent) {
+      await addHoursModal
+        .getByRole("spinbutton", { name: "Minutes" })
+        .fill(entry.minsSpent);
+    }
+
+    await addHoursModal.locator("#activity-type").click();
+    await page
+      .getByRole("option", {
+        name: "Gaining technical experience by doing my job",
+      })
+      .click();
+
+    await addHoursModal.getByLabel("Impact").fill("N/A");
+
+    await addHoursModal.getByRole("button", { name: "Add Hours" }).click();
+
+    await addHoursModal.waitFor({ state: "hidden" });
+
+    // Check if entries have been added
     try {
-      await expect(expectedRow).toBeVisible({ timeout: 5000 });
+      await expect(existingRow.first()).toBeVisible({ timeout: 5000 });
       console.log(`✅ ${entry.date} has been added`);
     } catch (error) {
-      console.error(`Couldn'tt find ${entry.date}`);
+      console.error(`Couldn't find ${entry.date}`);
       throw error;
     }
   }
